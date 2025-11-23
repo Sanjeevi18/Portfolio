8bbfd0210e6615b2c23839268b2575596fef2383
Sanjeevi18 <sanjeevigopalvijaya47@gmail.com>
2025-11-23 22:24:59 +0530

    Fix : changes in proccessing keys

--- portfolio/js/server.js
+++ portfolio/js/server.js
@@ -1,10 +1,10 @@
-// Load environment variables for local development (optional)
-require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
-
 const express = require("express");
 const path = require("path");
 const compression = require("compression");
 const cors = require("cors");
+
+// Load environment variables for local development (optional)
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
